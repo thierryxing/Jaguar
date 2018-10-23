8cb886cc1037b11a6887d03c04a70dcc5b32d86d
thierryxing <thierry.xing@gmail.com>
2018-10-23 19:03:06 +0800

    update gitlab to 4.6

--- config/initializers/gitlab_config.rb
+++ config/initializers/gitlab_config.rb
@@ -1,6 +1,6 @@
 Gitlab.configure do |config|
-  config.endpoint = 'http://git.wanmeizhensuo.com/api/v3' # API endpoint URL, default: ENV['GITLAB_API_ENDPOINT']
-  config.private_token = 'HTySY4s9G577a-w_xhCc' # user's private token or OAuth2 access token, default: ENV['GITLAB_API_PRIVATE_TOKEN']
+  config.endpoint = ENV['GITLAB_URL'] # API endpoint URL, default: ENV['GITLAB_API_ENDPOINT']
+  config.private_token = ENV['GITLAB_PRIVATE_TOKEN'] # user's private token or OAuth2 access token, default: ENV['GITLAB_API_PRIVATE_TOKEN']
   # Optional
   # config.user_agent   = 'Custom User Agent'          # user agent, default: 'Gitlab Ruby Gem [version]'
   # config.sudo         = 'user'                       # username for sudo mode, default: nil
